034f41f8d755fe6aee3ccb9aea126d47a3b916d9
Everita Vecbērza <everita.vecberza@gmail.com>
2023-02-28 18:22:35 +0200

    Read file

--- myArrayList/service/MainService.java
+++ myArrayList/service/MainService.java
@@ -1,6 +1,10 @@
 package service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import datastr.MyArrayList;
 import datastr.SortingType;
@@ -8,39 +12,62 @@ import datastr.SortingType;
 public class MainService {
 
 	public static void main(String[] args) {
+		System.setProperty("console.encoding", "UTF-8");
 		MyArrayList myList = new MyArrayList();
-		
+
 		try {
-		myList.append('a');		
-		myList.append('g');		
-		myList.append('e');		
-		
-		myList.print();  //age
-		myList.appendAtIndex('z', 0);	
-		myList.print(); //zage
-		
-		myList.deleteAtIndex(1);
-		myList.print(); //zge
-		System.out.println("Elements 1. indeksā - " + myList.getElementAtIndex(1));
-		
-		myList.append('a');		
-		myList.append('z');		
-		myList.append('j');		// ievietojot šeit, saraksts tiek palielināts
-		myList.append('z');		
-		
-		System.out.println(Arrays.toString(myList.search('z'))); //0 4 6
-		System.out.println(Arrays.toString(myList.searchForRightNeighbour('z'))); //g j
-		System.out.println(Arrays.toString(myList.sort(SortingType.ASC)));
-		System.out.println(Arrays.toString(myList.sort(SortingType.DESC)));
+			myList.append('a');
+			myList.append('g');
+			myList.append('e');
 
+			myList.print(); // age
+			myList.appendAtIndex('z', 0);
+			myList.print(); // zage
 
-		myList.makeEmpty();
-		myList.print();
-		myList.append('a');
-		
+			myList.deleteAtIndex(1);
+			myList.print(); // zge
+			System.out.println("Elements 1. indeksā - " + myList.getElementAtIndex(1));
+
+			myList.append('a');
+			myList.append('z');
+			myList.append('j'); // ievietojot šeit, saraksts tiek palielināts
+			myList.append('z');
+
+			System.out.println(Arrays.toString(myList.search('z'))); // 0 4 6
+			System.out.println(Arrays.toString(myList.searchForRightNeighbour('z'))); // g j
+			System.out.println(Arrays.toString(myList.sort(SortingType.ASC)));
+			System.out.println(Arrays.toString(myList.sort(SortingType.DESC)));
+
+			myList.makeEmpty();
+			myList.print();
+			myList.append('a');
+			
+			System.out.println("Darbības ar txt:");
+			MyArrayList listFromFile = createListFromFile("resources/numbers.txt");
+			listFromFile.append('š');
+			listFromFile.print();
+			System.out.println("žā");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	public static MyArrayList createListFromFile(String pathToFile) throws FileNotFoundException {
+		File myFile = new File(pathToFile);
+		FileInputStream myStream = new FileInputStream(myFile);
+		Scanner myScanner = new Scanner(myStream);
+		MyArrayList listFromFile = new MyArrayList();
+
+		while (myScanner.hasNext()) {
+			String line = myScanner.nextLine();
+			char element = line.charAt(0);
+			listFromFile.append(element);
+		}
+
+		myScanner.close();
+		return listFromFile;
 
 	}
 
